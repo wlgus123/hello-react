06df2b2b3e21c5cc769f9db759d4b6ce3f853df3
wlgus123 <mail30019794@gmail.com>
2024-10-27 14:02:51 +0900

    4.3 함수 컴포넌트로 구현해보기 - form, setForm 사용

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,15 +1,25 @@
 import { useState } from "react";
 
 const EventPractice = () => {
-    const [username, setUsername] = useState('');
-    const [message, setMessage] = useState('');
-    const onChangeUsername = e => setUsername(e.target.value);
-    const onChangeMessage = e => setMessage(e.target.value);
+    const [form, setForm] = useState({
+        username: '',
+        message: ''
+    });
+    const { username, message } = form;
+    const onChange = e => {
+        const nextForm = {
+            ...form,    // 기존의 form 내용을 이 자리에 복사
+            [e.target.name]: e.target.value // 원하는 값을 덮어 씌우기
+        };
+        setForm(nextForm);
+    };
 
     const onClick = () => {
         alert(username + ": " + message);
-        setUsername('');
-        setMessage('');
+        setForm({
+            username: '',
+            message: ''
+        })
     };
 
     // onKeyPress 이벤트 핸들링
@@ -27,14 +37,14 @@ const EventPractice = () => {
                 name="username"
                 placeholder="사용자명"
                 value={username}
-                onChange={onChangeUsername}
+                onChange={onChange}
             />
             <input
                 type="text"
                 name="message"
                 placeholder="아무거나 입력해 보세요"
                 value={message}
-                onChange={onChangeMessage}
+                onChange={onChange}
                 onKeyPress={onKeyPress}
             />
             <button onClick={onClick}>확인</button>
