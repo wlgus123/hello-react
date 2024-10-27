488fb49871c01af8696fa899c8e6ee03d33c0e9d
wlgus123 <mail30019794@gmail.com>
2024-10-27 12:44:15 +0900

    4.2.2-3 버튼을 누를 때 comment 값을 공백으로 설정

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -21,6 +21,14 @@ class EventPractice extends Component {
                         }
                     }
                 />
+                <button onClick={
+                    () => {
+                        alert(this.state.message);
+                        this.setState({
+                            message: ''
+                        });
+                    }
+                }>확인</button>
             </div>
         )
     }
