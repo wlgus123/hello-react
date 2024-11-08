c066724028ec050e384fb5711ed8835a68b1f6ce
wlgus123 <mail30019794@gmail.com>
2024-11-08 23:26:46 +0900

    5.3.4 컴포넌트에 ref 달고 내부 메서드 사용 - 에러 수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import ScrollBox from './ScrollBox';
 
 const App = () => {
+  const scrollBox = useRef();
   return (
     <div>
-      <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-      <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+      {/* this 사용 시 에러 발생 - 올바르게 참조 X. 따라서 useRef 사용하여 ref 관리 */}
+      <ScrollBox ref={scrollBox} />
+      <button onClick={() => scrollBox.current.scrollToBottom()}>
+        맨 밑으로
+      </button>
     </div>
   );
 };
