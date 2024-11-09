20e5cc10e2bd527e1c21224226a3db74d7b497e7
wlgus123 <mail30019794@gmail.com>
2024-11-09 23:36:15 +0900

    6.3 key

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,7 +1,7 @@
 const IterationSample = () => {
   const names = ['눈사람', '얼음', '눈', '바람'];
-  const nameList = names.map((name) => <li>{name}</li>); // map을 사용하여 리스트 출력
-  return <ul>{nameList}</ul>; // Warning: "key" prop이 없습니다.
+  const nameList = names.map((name, index) => <li key={index}>{name}</li>); // map을 사용하여 리스트 출력
+  return <ul>{nameList}</ul>; // Warning: "key" prop이 없습니다. -> li에 key 주면 해결
 };
 
 export default IterationSample;
