6698144a07fce01a222eeb28a4dff611dc4ed63d
wlgus123 <mail30019794@gmail.com>
2024-11-10 22:45:08 +0900

    6.4.1 초기 상태 설정하기

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 const IterationSample = () => {
-  const names = ['눈사람', '얼음', '눈', '바람'];
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>); // map을 사용하여 리스트 출력
-  return <ul>{nameList}</ul>; // Warning: "key" prop이 없습니다. -> li에 key 주면 해결
+  // useState를 사용하여 상태 설정
+  const [names, setNames] = useState([
+    { id: 1, text: '눈사람' },
+    { id: 2, text: '얼음' },
+    { id: 3, text: '눈' },
+    { id: 4, text: '바람' },
+  ]);
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5); // 새로운 항목을 차가할 때 사용할 id
+
+  // map을 사용하여 리스트 출력
+  const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  // -> Warning: "key" prop이 없습니다. -> li에 key 주면 해결
+  return <ul>{nameList}</ul>;
 };
 
 export default IterationSample;
