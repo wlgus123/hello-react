fb7ba7ebe4bdea9626740f316f061a3355c50a01
wlgus123 <mail30019794@gmail.com>
2024-11-10 22:54:49 +0900

    6.4.2 데이터 추가 기능 구현하기 - concat 사용

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -12,6 +12,15 @@ const IterationSample = () => {
   const [nextId, setNextId] = useState(5); // 새로운 항목을 차가할 때 사용할 id
 
   const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1); // nextId 값에 1을 더해준다.
+    setNames(nextNames); // names 값을 업데이트한다.
+    setInputText(''); // inputText를 비운다.
+  };
 
   // map을 사용하여 리스트 출력
   const namesList = names.map((name) => <li key={name.id}>{name.text}</li>);
@@ -20,7 +29,7 @@ const IterationSample = () => {
     <>
       {/* input, button 렌더링 */}
       <input value={inputText} onChange={onChange} />
-      <button>추가</button>
+      <button onClick={onClick}>추가</button>
       <ul>{namesList}</ul>
     </>
   );
